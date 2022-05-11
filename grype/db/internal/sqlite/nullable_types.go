44ec50506d5989e8c82dd0c151e3f86355c50a50
Weston Steimel <weston.steimel@anchore.com>
2022-05-11 16:45:32 +0100

    WIP v4 schema
    Signed-off-by: Weston Steimel <weston.steimel@anchore.com>

---
+++ grype/db/internal/sqlite/nullable_types.go
@@ -0,0 +1,33 @@
+package sqlite
+
+import (
+	"database/sql"
+	"encoding/json"
+)
+
+type NullString struct {
+	sql.NullString
+}
+
+func (v NullString) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.String)
+	}
+
+	return json.Marshal(nil)
+}
+
+func (v *NullString) UnmarshalJSON(data []byte) error {
+	// Unmarshalling into a pointer will let us detect null
+	var x *string
+	if err := json.Unmarshal(data, &x); err != nil {
+		return err
+	}
+	if x != nil {
+		v.Valid = true
+		v.String = *x
+	} else {
+		v.Valid = false
+	}
+	return nil
+}
